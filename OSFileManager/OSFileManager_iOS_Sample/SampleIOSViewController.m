32a34429efeed91b15c6e6f0de90657d84fb1a30
Ossey <xiaoyuan1314@me.com>
2017-07-23 13:36:16 +0800

    update total progress UI

--- OSFileManager/OSFileManager_iOS_Sample/SampleIOSViewController.m
+++ OSFileManager/OSFileManager_iOS_Sample/SampleIOSViewController.m
@@ -14,15 +14,15 @@
 #define kCopyDirectory [kDoucmentPath stringByAppendingPathComponent:@"copy"]
 
 @interface SampleIOSViewController ()
-@property (weak, nonatomic) IBOutlet UIProgressView *progressView;
+@property (weak, nonatomic) IBOutlet UIProgressView *progressView1;
+@property (weak, nonatomic) IBOutlet UIProgressView *progressView2;
+@property (weak, nonatomic) IBOutlet UIProgressView *totalProgress;
 
 @end
 
 @implementation SampleIOSViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
++ (void)load {
     NSFileManager *fileManager = [NSFileManager new];
     
     BOOL isExist = NO, isDirectory = NO;
@@ -32,26 +32,52 @@
     }
     
 }
-- (IBAction)documentBrower:(id)sender {
-    
-    FoldersViewController *vc = [[FoldersViewController alloc] initWithPath:kDoucmentPath];
-    [self.navigationController showViewController:vc sender:self];
-}
 
-- (IBAction)copyBtnClick:(id)sender {
+
+- (IBAction)samllFileCopy:(id)sender {
     
     NSURL *sourceURL = [NSURL fileURLWithPath:[kDoucmentPath stringByAppendingPathComponent:@"testcopy"]];
     NSURL *dstURL = [NSURL fileURLWithPath:kCopyDirectory];
     [[OSFileManager defaultManager] copyItemAtURL:sourceURL toURL:dstURL progress:^(NSProgress *progress) {
         NSLog(@"%f", progress.fractionCompleted);
         dispatch_async(dispatch_get_main_queue(), ^{
-            self.progressView.progress = progress.fractionCompleted;
+            self.progressView1.progress = progress.fractionCompleted;
         });
         
     } completionHandler:^(id<OSFileOperation> fileOperation, NSError *error) {
         NSLog(@"%ld", fileOperation.writeState);
     }];
 }
+- (IBAction)bigFileCopy:(id)sender {
+    
+    NSURL *sourceURL = [NSURL fileURLWithPath:[kDoucmentPath stringByAppendingPathComponent:@"testcopy_bigfile"]];
+    NSURL *dstURL = [NSURL fileURLWithPath:kCopyDirectory];
+    [[OSFileManager defaultManager] copyItemAtURL:sourceURL toURL:dstURL progress:^(NSProgress *progress) {
+        NSLog(@"%f", progress.fractionCompleted);
+        dispatch_async(dispatch_get_main_queue(), ^{
+            self.progressView2.progress = progress.fractionCompleted;
+        });
+    } completionHandler:^(id<OSFileOperation> fileOperation, NSError *error) {
+        NSLog(@"%ld", fileOperation.writeState);
+    }];
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    [OSFileManager defaultManager].totalProgressBlock = ^(NSProgress *progress) {
+      dispatch_async(dispatch_get_main_queue(), ^{
+          self.totalProgress.progress = progress.fractionCompleted;
+      });
+    };
+}
+- (IBAction)documentBrower:(id)sender {
+    
+    FoldersViewController *vc = [[FoldersViewController alloc] initWithPath:kDoucmentPath];
+    [self.navigationController showViewController:vc sender:self];
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
